a628645bd3733c32febe1ccb4b34a3e42f309a9c
Raghav Singhal <19d070049@iitb.ac.in>
2024-04-28 05:04:53 -0700

    added stuff

--- src/config.py
+++ src/config.py
@@ -71,7 +71,7 @@ class Config():
                     'num_train_data': 67972,
                     'early_stop': 4,
                     # training/validation/test parameters
-                    'batch_size': 2,
+                    'batch_size': 32,
                     'batch_gradient': 128,
                     'max_length': 512,
                     # 'gradient_accumulation_steps': 1,
